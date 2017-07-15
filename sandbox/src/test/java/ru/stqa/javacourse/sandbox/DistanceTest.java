49ef7174930c95f5fe35f6318eab3ddc85e7f18a
Olbar <Olegb1984@mail.ru>
2017-07-15 14:34:42 +0300

    Создан тестовый класс с тестом

---
+++ sandbox/src/test/java/ru/stqa/javacourse/sandbox/DistanceTest.java
@@ -0,0 +1,14 @@
+package ru.stqa.javacourse.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DistanceTest {
+
+    @Test
+    public void testDistance(){
+        Point p1 = new Point(0.0, 0.0);
+        Point p2 = new Point(4.0, 3.0);
+        Assert.assertEquals(p2.distance(p1),5.0);
+    }
+}
