731ea03a7a197a953442c65643013fa751668ff5
Olbar <Olegb1984@mail.ru>
2017-07-19 23:42:37 +0300

    разбил ApplicationManager на вспомогательные классы

--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/test/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/test/ContactCreationTests.java
@@ -8,9 +8,9 @@ public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
-        app.initContactCreation();
-        app.fillContactForm(new ContactData("Ivan", "Ivanovich", "Ivanov", "ivanov-i", "user", "roga i kopita", "Lenin st 5", "4565656", "+79213454545", "3454545", "3454546", "ivanov-i@mail.ru", "ivanov-i@gmail.com", "ivanov-i@yandex.ru", "no"));
-        app.submitContactCreation();
+        app.getGroupHelper().initContactCreation();
+        app.getGroupHelper().fillContactForm(new ContactData("Ivan", "Ivanovich", "Ivanov", "ivanov-i", "user", "roga i kopita", "Lenin st 5", "4565656", "+79213454545", "3454545", "3454546", "ivanov-i@mail.ru", "ivanov-i@gmail.com", "ivanov-i@yandex.ru", "no"));
+        app.getGroupHelper().submitContactCreation();
     }
 
 }
