0dcb7b6675051a398754e2609eb90237b79d2257
Olbar <Olegb1984@mail.ru>
2017-08-13 02:08:39 +0300

    6.5. Загрузка тестовых данных из файла

--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/test/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/test/ContactCreationTests.java
@@ -5,9 +5,13 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.javacourse.addressbook.model.ContactData;
 import ru.stqa.javacourse.addressbook.model.Contacts;
+import ru.stqa.javacourse.addressbook.model.GroupData;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,19 +23,20 @@ import static org.testng.Assert.assertEquals;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts(){
+    public Iterator<Object[]> validContacts() throws IOException {
         File photo = new File("src/test/resources/cat.jpg");
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {new ContactData().withFirstname("Ivan 1").withLastname("Ivanov 1").withCompanyAddress("Lenin street 6")
-                .withHomeNumber("2323453").withMobileNumber("4354653").withWorkNumber("899-23434").withPhoto(photo)});
-        list.add(new Object[] {new ContactData().withFirstname("Ivan 2").withLastname("Ivanov 2").withCompanyAddress("Lenin street 7")
-                .withHomeNumber("2334523").withMobileNumber("43345353").withWorkNumber("899-234334").withPhoto(photo)});
-        list.add(new Object[] {new ContactData().withFirstname("Ivan 3").withLastname("Ivanov 3").withCompanyAddress("Lenin street 8")
-                .withHomeNumber("2324423").withMobileNumber("43254353").withWorkNumber("899-234").withPhoto(photo)});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[]{new ContactData().withFirstname(split[0])
+                    .withLastname(split[1]).withCompanyAddress(split[2])
+                    .withHomeNumber(split[3]).withMobileNumber(split[4]).withWorkNumber(split[5]).withPhoto(photo)});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
-
-
 
 
     @Test(dataProvider = "validContacts")
