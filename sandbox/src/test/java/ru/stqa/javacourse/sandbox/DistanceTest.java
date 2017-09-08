7af89098f1087560dd7317adff4013b053616fe6
Olbar <Olegb1984@mail.ru>
2017-09-08 23:12:03 +0300

    испорчен тест

--- sandbox/src/test/java/ru/stqa/javacourse/sandbox/DistanceTest.java
+++ sandbox/src/test/java/ru/stqa/javacourse/sandbox/DistanceTest.java
@@ -9,6 +9,6 @@ public class DistanceTest {
     public void testDistance(){
         Point p1 = new Point(0.0, 0.0);
         Point p2 = new Point(4.0, 3.0);
-        Assert.assertEquals(p2.distance(p1),5.0);
+        Assert.assertEquals(p2.distance(p1),6.0);
     }
 }
